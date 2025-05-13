cf7b6ce84c072504759202f5ae36458e8cb3239d
Brokewala <brokewala@gmail.com>
2025-05-13 23:44:12 +0300

    wal

--- app/admin/categories/page.tsx
+++ app/admin/categories/page.tsx
@@ -12,7 +12,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
-// was --------dd
+// was --------dde
 const Page = () => {
   const route = useRouter();
 
